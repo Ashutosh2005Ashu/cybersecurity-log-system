6c4c9e0486ee8a03cdb41b2cb314c7ec0c1036c9
Ashutosh Awasthi <ashutoshvawasthi19@gmail.com>
2026-02-08 16:32:51 +0530

    added user entity ,role enum and userRepository with jpa configuration

---
+++ backend/src/main/java/com/ashutosh/cybersec/auth/entity/User.java
@@ -0,0 +1,29 @@
+package com.ashutosh.cybersec.auth.entity;
+import com.ashutosh.cybersec.common.enums.Role;
+import jakarta.persistence.*;
+import lombok.*;
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
---
+++ backend/src/main/java/com/ashutosh/cybersec/common/enums/Role.java
@@ -0,0 +1,7 @@
+package com.ashutosh.cybersec.common.enums;
+
+public enum Role {
+    ADMIN,
+    ANALYST
+}
+
