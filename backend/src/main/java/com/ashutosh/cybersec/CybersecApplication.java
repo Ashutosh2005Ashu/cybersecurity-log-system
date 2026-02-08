c4f326c91b405a59b05f04aec4b09f812a3cefa7
Ashutosh Awasthi <ashutoshvawasthi19@gmail.com>
2026-02-08 15:50:34 +0530

    inital project structure

---
+++ backend/src/main/java/com/ashutosh/cybersec/CybersecApplication.java
@@ -0,0 +1,13 @@
+package com.ashutosh.cybersec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CybersecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CybersecApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/ashutosh/cybersec/CybersecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashutosh.cybersec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CybersecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
